7c442c380ce743f0f72dd850a73303f9f0b7f39c
Jakub Malota <jakub.malota08@gmail.com>
2026-01-26 16:42:56 +0100

    version_9

--- eshop_makercord/src/components/ProductsOverview.js
+++ eshop_makercord/src/components/ProductsOverview.js
@@ -5,13 +5,14 @@ const ProductsOverview = () => {
     return (
         <div className="products-overview">
             {data.map((product) => {
-                const { image, name, description, price } = product;
+                const { id, image, name, description, price } = product;
                 return (
-                    <div className="product-item" key={product.id}>
+                    <div className="product-item" key={id}>
                         <img src={image} alt={name} />
                         <h3>{name}</h3>
                         <p>{description}</p>
                         <p>Price: ${price}</p>
+                        <button>Add to Cart</button>
                     </div>
                 );
             })}
