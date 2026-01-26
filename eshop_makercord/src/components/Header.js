2a464d7db4db44d2e4bdf155ae50c89af2faa23f
Jakub Malota <jakub.malota08@gmail.com>
2026-01-26 09:11:39 +0100

    version_6

--- eshop_makercord/src/components/Header.js
+++ eshop_makercord/src/components/Header.js
@@ -1,21 +1,25 @@
 import "./Header.css";
+import logo from "../images/logo.png";
+import contact_icon from "../images/contact_icon.png";
+import cart_icon from "../images/cart_icon.png";
+import profile_icon from "../images/profile_icon.png";
+import translation_icon from "../images/translation_icon.png";
 
 const Header = () => {
   return (
     <header>
       <div className="header-top">
-        <img src="../images/logo.png" alt="" />
+        <img src={logo} className="logo" alt="Logo" />
         <input id="searchbar" type="search" placeholder="Search..." />
         <ul className="nav-links">
-            <li><a href="."><img src="../images/contact_icon.png" alt="" /></a></li>
-            <li><a href="."><img src="" alt="" /></a></li>
-            <li><a href="."><img src="" alt="" /></a></li>
-            <li><a href="."><img src="" alt="" /></a></li>
+          <li><a href="."><img src={contact_icon} alt="Contact" /></a></li>
+          <li><a href="."><img src={cart_icon} alt="Cart" /></a></li>
+          <li><a href="."><img src={profile_icon} alt="Profile" /></a></li>
+          <li><a href="."><img src={translation_icon} alt="Translation" /></a></li>
         </ul>
       </div>
       <div className="header-bottom" />
     </header>
   );
 };
-
 export default Header;
