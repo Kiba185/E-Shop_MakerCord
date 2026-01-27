4604d1925060eab9eff9ab4b6efb36bad67ce9f9
Jakub Malota <jakub.malota08@gmail.com>
2026-01-27 16:47:59 +0100

    version_12

--- eshop_makercord/src/components/Footer.js
+++ eshop_makercord/src/components/Footer.js
@@ -3,11 +3,11 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer>
-            <div className="footer-links">
-                <a href="/about">About Us</a>
-                <a href="/contact">Contact</a>
-                <a href="/privacy">Privacy Policy</a>
-            </div>
+            <ul className="footer-links">
+                <li><a href="/about">About Us</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/privacy">Privacy Policy</a></li>
+            </ul>
             <p>© 2024 MakerCord. All rights reserved.</p>   
         </footer>
     );
