4ab46cc6aecaf53fb0d54a0a35b46f3b02a70e07
Jakub Malota <jakub.malota08@gmail.com>
2026-01-30 17:01:55 +0100

    version_14

--- eshop_makercord/src/App.js
+++ eshop_makercord/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Contact from './pages/Contact';
@@ -9,18 +9,16 @@ import Error from './pages/Error';
 import SharedLayout from './pages/SharedLayout';
 
 const App = () => {
-    return <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<SharedLayout />}>
-                <Route index element={<Home />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/user-profile" element={<UserProfile />} />
-                <Route path="*" element={<Error />} />
-            </Route>
-        </Routes>
-    </BrowserRouter>;
+    return <Routes>
+        <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/user-profile" element={<UserProfile />} />
+            <Route path="*" element={<Error />} />
+        </Route>
+    </Routes>;
   };
 
   export default App;
