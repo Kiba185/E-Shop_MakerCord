abd095661a980f2c1d2310d5f9ab643ee62156ef
Jakub Malota <jakub.malota08@gmail.com>
2026-05-05 11:28:46 +0000

    version_61

--- eshop_makercord/src/components/ProductCard.js
+++ eshop_makercord/src/components/ProductCard.js
@@ -8,7 +8,7 @@ const ProductCard = ({ product }) => {
       <div className="product-card">
         <Link to={`/product/${product.id}`}>
           <img src={image} alt={name} />
-          <h3>{name}</h3>
+          <h2>{name}</h2>
           <p>{description}</p>
           <p className="price">{price}<span className="currency">Kč</span></p>
         </Link>
